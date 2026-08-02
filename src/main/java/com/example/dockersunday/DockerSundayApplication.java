c6b69438c6a72927068178d24baecf2ceda40f0a
jharana <jharanakc38@gmail.com>
2026-08-02 10:39:59 +0900

    first

---
+++ src/main/java/com/example/dockersunday/DockerSundayApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockersunday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSundayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSundayApplication.class, args);
+    }
+
+}
